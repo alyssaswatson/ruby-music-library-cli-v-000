1e34949a23441bbb60b342739f869a3ed3597a78
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-11-21 23:50:42 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -26,7 +26,7 @@ class MusicLibraryController
     elsif input == "list genres"
       list_genres
     elsif input == "list artist"
-      list_artist
+      list_songs_by_artist
     elsif input == "list genre"
       list_genre
     elsif input == "play song"
